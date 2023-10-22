1295c9b7a5f3207dc5a4d2a9fe6023d56d179db8
unknown <gunaorusty@gmail.com>
2023-10-22 13:24:25 +0800

    add initial setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { faSquare } from '@fortawesome/free-solid-svg-icons'
+
+  import Navbar from './components/Navbar.vue'
+  import Home from './components/Home.vue'
+
+</script>
+
+<template>
+  <Navbar />
+  <Home />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+
+import './main.css'
+
+// BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import App from './App.vue'
+
+// FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
+const app = createApp(App);
+
+app.component('icon', FontAwesomeIcon);
+
+app.mount('#app');
